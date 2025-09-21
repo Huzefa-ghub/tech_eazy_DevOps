90419f5cb52b60be8e219862ec267e628e220d0b
Huzefa <huzefashyk2672@gmail.com>
2025-09-21 13:10:46 +0530

    Update user_data.sh

--- script/user_data.sh
+++ script/user_data.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y openjdk-21-jdk maven git
 
 cd /home/ubuntu
-git clone https://github.com/Trainings-TechEazy/test-repo-for-devops.git app
+git clone https://github.com/Huzefa-ghub/tech_eazy_DevOps.git app
 cd app
 mvn clean package
 sudo nohup java -jar target/hellomvc-0.0.1-SNAPSHOT.jar --server.port=80 > /home/ubuntu/app.log 2>&1 &
